6a28e837d7ca9947d918d1e2ade8798de9cb9c8f
RuStyC0der <miller_maers@tutanota.com>
2019-11-14 15:51:56 +0200

    added phigure image

---
+++ tmp.py
@@ -0,0 +1,10 @@
+import re
+import os
+if __name__ == '__main__':
+    # with open("calc.ui", "r") as ui:
+    #     a = ui.read()
+    #     z = re.findall("lineEdit_\d+", a)
+    #     z = set(z)
+    #     for i in z:
+    #         print("self.ui.", i, ".", "setValidator(self.validator)", sep="")
+    print(os.listdir())
--- tri.py
+++ tri.py
@@ -1,6 +1,9 @@
 from PyQt5.QtWidgets import *
 from PyQt5 import uic, QtGui
 
+from PyQt5.QtGui import QRegExpValidator, QPixmap
+from PyQt5.QtCore import QRegExp
+
 import calc_model
 
 
@@ -16,57 +19,138 @@ class triangle(QMainWindow):
         self._triangle_button_group.addButton(self.ui.radioButton_5)
         self._triangle_button_group.addButton(self.ui.radioButton_6)
 
+        self._triangle_button_group.buttonClicked.connect(self.triangle_swich_menus)
+        self.ui.pushButton_2.clicked.connect(self.calculate_triangle)
+
+        self.validator = QRegExpValidator(QRegExp(r"\d+[\.,]?\d*"))
+
+        #  validate for all line. I do not write this code manually :)
+        self.ui.lineEdit_50.setValidator(self.validator)
+        self.ui.lineEdit.setValidator(self.validator)
+        self.ui.lineEdit_45.setValidator(self.validator)
+        self.ui.lineEdit_5.setValidator(self.validator)
+        self.ui.lineEdit_46.setValidator(self.validator)
+        self.ui.lineEdit_77.setValidator(self.validator)
+        self.ui.lineEdit_29.setValidator(self.validator)
+        self.ui.lineEdit_39.setValidator(self.validator)
+        self.ui.lineEdit_83.setValidator(self.validator)
+        self.ui.lineEdit_51.setValidator(self.validator)
+        self.ui.lineEdit_71.setValidator(self.validator)
+        self.ui.lineEdit_56.setValidator(self.validator)
+        self.ui.lineEdit_12.setValidator(self.validator)
+        self.ui.lineEdit_52.setValidator(self.validator)
+        self.ui.lineEdit_28.setValidator(self.validator)
+        self.ui.lineEdit_90.setValidator(self.validator)
+        self.ui.lineEdit_30.setValidator(self.validator)
+        self.ui.lineEdit_9.setValidator(self.validator)
+        self.ui.lineEdit_85.setValidator(self.validator)
+        self.ui.lineEdit_32.setValidator(self.validator)
+        self.ui.lineEdit_24.setValidator(self.validator)
+        self.ui.lineEdit_26.setValidator(self.validator)
+        self.ui.lineEdit_11.setValidator(self.validator)
+        self.ui.lineEdit_25.setValidator(self.validator)
+        self.ui.lineEdit_36.setValidator(self.validator)
+        self.ui.lineEdit_64.setValidator(self.validator)
+        self.ui.lineEdit_280.setValidator(self.validator)
+        self.ui.lineEdit_35.setValidator(self.validator)
+        self.ui.lineEdit_273.setValidator(self.validator)
+        self.ui.lineEdit_274.setValidator(self.validator)
+        self.ui.lineEdit_13.setValidator(self.validator)
+        self.ui.lineEdit_34.setValidator(self.validator)
+        self.ui.lineEdit_14.setValidator(self.validator)
+        self.ui.lineEdit_23.setValidator(self.validator)
+        self.ui.lineEdit_31.setValidator(self.validator)
+        self.ui.lineEdit_270.setValidator(self.validator)
+        self.ui.lineEdit_271.setValidator(self.validator)
+        self.ui.lineEdit_7.setValidator(self.validator)
+        self.ui.lineEdit_277.setValidator(self.validator)
+        self.ui.lineEdit_37.setValidator(self.validator)
+        self.ui.lineEdit_27.setValidator(self.validator)
+        self.ui.lineEdit_10.setValidator(self.validator)
+        self.ui.lineEdit_63.setValidator(self.validator)
+        self.ui.lineEdit_60.setValidator(self.validator)
+        self.ui.lineEdit_79.setValidator(self.validator)
+        self.ui.lineEdit_279.setValidator(self.validator)
+        self.ui.lineEdit_84.setValidator(self.validator)
+        self.ui.lineEdit_38.setValidator(self.validator)
+        self.ui.lineEdit_3.setValidator(self.validator)
+        self.ui.lineEdit_49.setValidator(self.validator)
+        self.ui.lineEdit_62.setValidator(self.validator)
+        self.ui.lineEdit_6.setValidator(self.validator)
+        self.ui.lineEdit_4.setValidator(self.validator)
+        self.ui.lineEdit_59.setValidator(self.validator)
+        self.ui.lineEdit_8.setValidator(self.validator)
+        self.ui.lineEdit_67.setValidator(self.validator)
+        self.ui.lineEdit_2.setValidator(self.validator)
+        self.ui.lineEdit_74.setValidator(self.validator)
+        self.ui.lineEdit_70.setValidator(self.validator)
+        self.ui.lineEdit_276.setValidator(self.validator)
+        self.ui.lineEdit_22.setValidator(self.validator)
+        self.ui.lineEdit_33.setValidator(self.validator)
+        self.ui.lineEdit_91.setValidator(self.validator)
+        self.ui.lineEdit_272.setValidator(self.validator)
+        self.ui.lineEdit_57.setValidator(self.validator)
+
+
         self.model = calc_model.Area_calc()
+        self.setWindowIcon(QtGui.QIcon('./media/window_icon.png'))
 
         self.setFixedSize(self.size())
         self.ui.stackedWidget.setCurrentIndex(1)
-        self.setWindowIcon(QtGui.QIcon('./media/window_icon.png'))
-
-        self._triangle_button_group.buttonClicked.connect(self.triangle_swich_menus)
-        self.ui.pushButton_2.clicked.connect(self.but)
         self.typeR = "1"
+        self.ui.lineEdit_26.setReadOnly(True)
 
-    def but(self):
-        if self.typeR == "1":
-            a = int(self.ui.lineEdit.text())
-            b = int(self.ui.lineEdit_2.text())
-            c = int(self.ui.lineEdit_3.text())
-            res = self.model.triangle.geron(a,b,c)
-        elif self.typeR == "2":
-            a = int(self.ui.lineEdit_4.text())
-            b = int(self.ui.lineEdit_5.text())
-            c = int(self.ui.lineEdit_6.text())
-            res = self.model.triangle.two_side_one_angle(a,b,c)
-        elif self.typeR == "3":
-            a = int(self.ui.lineEdit_7.text())
-            b = int(self.ui.lineEdit_8.text())
-            res = self.model.triangle.side_height(a,b)
-        elif self.typeR == "4":
-            a = int(self.ui.lineEdit_9.text())
-            b = int(self.ui.lineEdit_10.text())
-            c = int(self.ui.lineEdit_11.text())
-            d = int(self.ui.lineEdit_12.text())
-            res = self.model.triangle.thre_side_described_circle(a,b,c,d)
-        elif self.typeR == "5":
-            a = int(self.ui.lineEdit_13.text())
-            b = int(self.ui.lineEdit_14.text())
-            c = int(self.ui.lineEdit_15.text())
-            d = int(self.ui.lineEdit_16.text())
-            res = self.model.triangle.tree_side_inscribed_circle(a,b,c,d)
-        elif self.typeR == "6":
-            a = int(self.ui.lineEdit_17.text())
-            b = int(self.ui.lineEdit_18.text())
-            c = int(self.ui.lineEdit_19.text())    
-            res = self.model.triangle.one_side_two_angle(a,b,c)
+        # pixmap = QPixmap('./media/phigures/circle/довжина.jpg')
+        # self.ui.label_3.setPixmap(pixmap)
 
-        self.ui.lineEdit_21.setText(str(res))
+
+    def calculate_triangle(self):
+        try:
+            if self.typeR == "1":
+                a = float(self.ui.lineEdit.text().replace(",", "."))
+                b = float(self.ui.lineEdit_2.text().replace(",", "."))
+                c = float(self.ui.lineEdit_3.text().replace(",", "."))
+                res = self.model.triangle.geron(a,b,c)
+            elif self.typeR == "2":
+                a = float(self.ui.lineEdit_4.text().replace(",", "."))
+                b = float(self.ui.lineEdit_5.text().replace(",", "."))
+                c = float(self.ui.lineEdit_6.text().replace(",", "."))
+                res = self.model.triangle.two_side_one_angle(a,b,c)
+            elif self.typeR == "3":
+                a = float(self.ui.lineEdit_7.text().replace(",", "."))
+                b = float(self.ui.lineEdit_8.text().replace(",", "."))
+                res = self.model.triangle.side_height(a,b)
+            elif self.typeR == "4":
+                a = float(self.ui.lineEdit_9.text().replace(",", "."))
+                b = float(self.ui.lineEdit_10.text().replace(",", "."))
+                c = float(self.ui.lineEdit_11.text().replace(",", "."))
+                d = float(self.ui.lineEdit_12.text().replace(",", "."))
+                res = self.model.triangle.thre_side_described_circle(a,b,c,d)
+            elif self.typeR == "5":
+                a = float(self.ui.lineEdit_13.text().replace(",", "."))
+                b = float(self.ui.lineEdit_14.text().replace(",", "."))
+                c = float(self.ui.lineEdit_15.text().replace(",", "."))
+                d = float(self.ui.lineEdit_16.text().replace(",", "."))
+                res = self.model.triangle.tree_side_inscribed_circle(a,b,c,d)
+            elif self.typeR == "6":
+                a = float(self.ui.lineEdit_17.text().replace(",", "."))
+                b = float(self.ui.lineEdit_18.text().replace(",", "."))
+                c = float(self.ui.lineEdit_19.text().replace(",", "."))
+                res = self.model.triangle.one_side_two_angle(a,b,c)
+        except ValueError:
+            print("error")
+            return
+
+        self.ui.lineEdit_26.setText(str(res))
         print(res)
 
     def triangle_swich_menus(self, button):
         matchicg_dict = {"1":1, "2":0, "3":2, "4":3, "5":4, "6":5}
+        path_dict = {"1":"./media/phigures/triangle/Герон.jpg", "2":"./media/phigures/triangle/довжини двох сторін і кут між ними.png", "3":"./media/phigures/triangle/довжина сторони  і опущена висота.png", "4":"./media/phigures/triangle/довжина трьох сторін  і радіус вписаного кола.png", "5":"./media/phigures/triangle/довжина трьох сторін  і радіус описаного кола.png", "6":"./media/phigures/triangle/довжина одного боку  і двох кутыв.png"}
         self.typeR = button.text()[0]
         self.ui.stackedWidget.setCurrentIndex(matchicg_dict[self.typeR])
-
+        pixmap = QPixmap(path_dict[self.typeR])
+        self.ui.label_3.setPixmap(pixmap)
 
 if __name__ == '__main__':
     app = QApplication([])
